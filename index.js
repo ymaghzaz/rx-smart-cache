0beae58be57556ed5f90a214d87b3369593c62b2
youssef maghzaz <youssefmaghzaz@gmail.com>
2018-10-24 01:08:12 +0200

    handle function with params

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const {
   Observable
 } = require("rxjs");
 const { map, filter, flatMap, mergeMap } = require("rxjs/operators");
+const generateID = require("./generateID");
 
 const partner1 = params =>
   new Observable(obs => {
@@ -34,7 +35,12 @@ const partner3 = params =>
 const store = {};
 const notification = {};
 
-const dispatch = (id, obs) => {
+const getId = (obsId, params) => {
+  return generateID({ obsId, params });
+};
+
+const dispatch = (obsId, obs, params) => {
+  const id = getId(obsId, params);
   if (!store[id]) {
     store[id] = obs;
     notification[id] = new BehaviorSubject({
@@ -80,8 +86,8 @@ const processStreamCache = dataStream$ => {
     });
   });
 };
-dispatch("partner1", partner1());
-dispatch("partner2", partner2());
+dispatch("partner1", partner1(1), 1);
+dispatch("partner2", partner2(1), 1);
 dispatch("partner1", partner1());
 dispatch("partner2", partner2());
 dispatch("partner1", partner1());
@@ -93,16 +99,24 @@ const streamCache$ = buildCache();
 processStreamCache(streamCache$);
 
 setTimeout(async () => {
-  notification.partner1.subscribe(partner1_data => {
+  notification[getId("partner1")].subscribe(partner1_data => {
     if (partner1_data.state === "End") {
       console.log("partner1_data", partner1_data);
     }
   });
 }, 2000);
 
+setTimeout(async () => {
+  notification[getId("partner1", 1)].subscribe(partner1_data => {
+    if (partner1_data.state === "End") {
+      console.log("partner1_data with 1", partner1_data);
+    }
+  });
+}, 2000);
+
 setTimeout(() => {
   const start = Date.now();
-  notification.partner2.subscribe(partner2_data => {
+  notification[getId("partner2")].subscribe(partner2_data => {
     if (partner2_data.state === "End") {
       const end = Date.now();
       console.log("after", (end - start) / 1000, " second");
@@ -113,7 +127,7 @@ setTimeout(() => {
 
 setTimeout(() => {
   const start = Date.now();
-  notification.partner1.subscribe(partner1_data => {
+  notification[getId("partner1")].subscribe(partner1_data => {
     if (partner1_data.state === "End") {
       const end = Date.now();
       console.log("after", (end - start) / 1000, " second");
